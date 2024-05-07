38b147251582e07c442de4077a1a13c096e6a7b0
Azeez Saka <saoconcept1@gmail.com>
2024-05-07 09:57:45 -0700

    Added task_0/dashboards/src/, task_0/dashboard/src/App.css, task_0/dashboard/src/App.js files

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import holberton_logo from "./holberton-logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={holberton_logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
