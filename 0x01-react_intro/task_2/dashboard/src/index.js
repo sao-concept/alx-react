61588ae4c7927caf83af811737fa9f9f16ce8396
Azeez Saka <saoconcept1@gmail.com>
2024-05-07 11:15:42 -0700

    Added dashboard/src/ dashboard/src/utils.js dashboard/src/Notifications.js dashboard/src/Notifications.css files

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -7,10 +7,10 @@ import Notifications from "./Notifications";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
     <div className="root-notifications">
       <Notifications />
     </div>
+    <App />
   </React.StrictMode>
 );
 
